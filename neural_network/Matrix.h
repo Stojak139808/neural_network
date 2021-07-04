0a3a479a82b7bd9996fccbef47d23793cc2f4681
Kacper Stojek <aedang01@gmail.com>
2021-07-04 21:57:36 +0200

    added matrix.h, which includes a matrix class

---
+++ neural_network/Matrix.h
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+class Matrix {
+
+public:
+	int** matrix;
+	int* rows;
+	int* columns;
+
+	Matrix(int number_of_rows, int number_of_columns) {
+		rows = new int;
+		columns = new int;
+		*rows = number_of_rows;
+		*columns = number_of_columns;
+		matrix = new int* [*rows];
+		for (int i = 0; i < *rows; ++i) {
+			*(matrix + i) = new int[*columns];
+			for (int j = 0; j < *columns; ++j) {
+				*(*(matrix + i) + j) = 0;
+			}
+		}
+
+	}
+
+	void print_matrix() {
+
+		cout << "rows: " << *rows << " columns: " << *columns << "\n";
+		for (int i = 0; i < *rows; ++i) {
+			for (int j = 0; j < *columns; ++j) {
+				cout << *(*(matrix + i) + j) << " ";
+			}
+			cout << "\n";
+		}
+	}
+
+	void fill(int x) {
+		for (int j = 0; j < *columns; ++j) {
+			for (int i = 0; i < *rows; ++i) {
+				*(*(matrix + i) + j) = x;
+			}
+		}
+	}
+
+	Matrix operator + (const Matrix& x) {
+		//this + x = y
+		if (*(x.rows) == *(this->rows) and *(x.columns) == *(this->columns)) {
+			Matrix y(*(this->rows), *(this->columns));
+			for (int i = 0; i < *rows; ++i) {
+				for (int j = 0; j < *columns; ++j) {
+					y.matrix[i][j] = x.matrix[i][j] + this->matrix[i][j];
+				}
+			}
+
+			return y;
+		}
+		else {
+			cout << "dimensions don't match, cannot add\n";
+		}
+	}
+
+	Matrix operator - (const Matrix& x) {
+		//this + x = y
+		if (*(x.rows) == *(this->rows) and *(x.columns) == *(this->columns)) {
+			Matrix y(*(this->rows), *(this->columns));
+			for (int i = 0; i < *rows; ++i) {
+				for (int j = 0; j < *columns; ++j) {
+					y.matrix[i][j] = this->matrix[i][j] - x.matrix[i][j];
+				}
+			}
+
+			return y;
+		}
+		else {
+			cout << "dimensions don't match, cannot subtract\n";
+		}
+	}
+
+	Matrix operator * (const Matrix& x) {
+		//this * x = y
+		if (*(this->columns) == *(x.rows)) {
+			Matrix y(*(this->rows), *(x.columns));
+			for (int i = 0; i < *(y.rows); ++i) {
+				for (int j = 0; j < *(y.columns); ++j) {
+					for (int n = 0; n < *(this->columns); ++n) {
+						y.matrix[i][j] += this->matrix[i][n] * x.matrix[n][j];
+					}
+				}
+			}
+			return y;
+		}
+		else {
+			cout << "dimensions don't match, cannot multiply\n";
+		}
+	}
+};
